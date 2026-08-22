7d3d69518838783c847b4e0d0eb0650c1923a831
Majid Ghafouri <majid.ghafouri@Gmail.com>
2026-08-23 01:30:11 +0200

    Fix regex escaping in template literal for article page language pills

--- src/lib/blog-page.ts
+++ src/lib/blog-page.ts
@@ -253,7 +253,7 @@ export function renderPostPage(
       document.querySelectorAll(".lang-pill[href]").forEach(function (a) {
         a.addEventListener("click", function () {
           var href = this.getAttribute("href");
-          var langMatch = href.match(/blog\/([^\/]+)-([a-z]{2})$/);
+          var langMatch = href.match(/blog\\/([^\\/]+)-([a-z]{2})$/);
           if (langMatch && langMatch[2] && i18n && i18n.switchTo) {
             i18n.switchTo(langMatch[2]);
           }
